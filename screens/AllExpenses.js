1f318457561dba66612a3c45597dbbf5f67cc3cb
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-05-02 00:52:45 -0500

    add starting setup

---
+++ screens/AllExpenses.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text } from "react-native"
+
+function AllExpenses() {
+    return (
+        <Text>AllExpenses page</Text>
+    )
+}
+
+export default AllExpenses
+
+const styles = StyleSheet.create({})
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text } from "react-native"
+
+function RecentExpenses() {
+    return (
+        <Text>RecentExpenses page</Text>
+    )
+}
+
+export default RecentExpenses
+
+const styles = StyleSheet.create({})
