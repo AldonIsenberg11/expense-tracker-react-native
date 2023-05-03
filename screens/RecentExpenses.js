7035f7ae5ff1de6165ce2362718485290d16e88d
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-05-03 14:01:48 -0500

    Fetch backend data from Firebase

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,21 @@
 import { StyleSheet } from "react-native"
+import { useContext, useEffect } from "react"
+
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput"
-import { useContext } from "react"
 import { ExpensesContext } from "../store/expenses-context"
 import { getDateMinusDays } from "../util/date"
+import { fetchExpenses } from "../util/http"
 
 function RecentExpenses() {
     const expensesCtx = useContext(ExpensesContext)
+
+    useEffect(() => {
+        async function getExpenses() {
+            const expenses = await fetchExpenses()
+        }
+
+        getExpenses()
+    }, [])
 
     const recentExpenses = expensesCtx.expenses.filter(expense => {
         const today = new Date()
