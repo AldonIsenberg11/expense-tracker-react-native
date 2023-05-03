77d928b2fbe9490018f5d15be625e9e208fc68f3
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-05-02 19:32:13 -0500

    finishing touches

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,18 +1,24 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 import { GlobalStyles } from "../../constants/styles";
 
-function ExpensesOutput({expenses, expensesPeriod}) {
-  return (
-    <View style={styles.container}>
-        <ExpensesSummary 
-            expenses={expenses}
-            periodName={expensesPeriod}
-        />
-        <ExpensesList expenses={expenses}/>
-    </View>
-  );
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+    let content = <Text style={styles.infoText}>{fallbackText}</Text>
+
+    if (expenses.length > 0) {
+        content = <ExpensesList expenses={expenses} />
+    }
+
+    return (
+        <View style={styles.container}>
+            <ExpensesSummary
+                expenses={expenses}
+                periodName={expensesPeriod}
+            />
+            {content}
+        </View>
+    );
 }
 
 export default ExpensesOutput;
@@ -24,5 +30,11 @@ const styles = StyleSheet.create({
         paddingTop: 24,
         paddingBottom: 0,
         backgroundColor: GlobalStyles.colors.primary700,
+    },
+    infoText: {
+        color: 'white',
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 32,
     },
 });
